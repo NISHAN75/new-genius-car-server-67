d145f505f5e8555c988036e1f8b33ba7504fc0cb
nishandas <nishandas880@gmail.com>
2022-04-25 01:01:56 +0600

    server add

---
+++ index.js
@@ -0,0 +1,68 @@
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+require("dotenv").config();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+// add connection
+
+const uri = `mongodb+srv://${process.env.DB_BUCKET}:${process.env.DB_KEY}@cluster0.dxvcg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+  try {
+    await client.connect();
+    const serviceCollection = client.db("geniusCar").collection("service");
+
+    //  read api
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+    app.get("/service/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const service = await serviceCollection.findOne(query);
+      res.send(service);
+    });
+
+    // post api
+    app.post("/service", async (req, res) => {
+      const newService = req.body;
+      const result = await serviceCollection.insertOne(newService);
+      res.send(result);
+    });
+
+    // DELETE
+    app.delete("/service/:id", async (req, res) => {
+      const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await serviceCollection.deleteOne(query);
+        res.send(result)
+    });
+  } finally {
+    console.log("clear");
+  }
+}
+run();
+
+// read api
+
+app.get("/", (req, res) => {
+  res.send("Running Genius Server");
+});
+
+app.listen(port, () => {
+  console.log("listening our Port", port);
+});
